fd41534a3a94e26fca12fd9bd6b7f8bc505c970e
David Tumpowsky <tumpowskydavid@gmail.com>
2018-07-13 16:07:08 -0700

    connect firebase to app

---
+++ src/components/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+  // Initialize Firebase
+const config = {
+    apiKey: "AIzaSyDKcLAPHQMLOJQ37pHMUGHecfIjTqEyUFY",
+    authDomain: "activity-tracker-a3fe9.firebaseapp.com",
+    databaseURL: "https://activity-tracker-a3fe9.firebaseio.com",
+    projectId: "activity-tracker-a3fe9",
+    storageBucket: "activity-tracker-a3fe9.appspot.com",
+    messagingSenderId: "901708571430"
+  };
+firebase.initializeApp(config);
+
+export default firebase;
