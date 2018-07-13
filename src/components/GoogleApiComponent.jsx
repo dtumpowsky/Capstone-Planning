0b9500e0ed861721508fb418f051e4b0bb7b9710
David Tumpowsky <tumpowskydavid@gmail.com>
2018-07-13 13:33:17 -0700

    continuing to work with Google Api

--- src/components/GoogleApiComponent.jsx
+++ src/components/GoogleApiComponent.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import apiKey from './../../.env';
+import Map from './Map.jsx';
 
 
-export class Container extends React.Component {
+class GoogleApiComponent extends React.Component {
   render() {
     const style = {
       width: '100vw',
       height: '100vh'
-    }
+    };
     return (
       <div style={style}>
         <Map google={this.props.google}
-          />
+        />
       </div>
-    )
+    );
   }
 }
 
 export default GoogleApiComponent({
   apiKey: apiKey
-})(Container)
+})(Container);
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-export class Map extends React.Component {
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const {lat, lng} = this.props.initialCenter;
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng
+      }
+    }
+  }
+
   loadMap() {
     if (this.props && this.props.google) {
       // google is available
       const {google} = this.props;
       const maps = google.maps;
 
+      const {lat, lng} = this.state.currentLocation;
+
       const mapRef = this.refs.map;
       const node = ReactDOM.findDOMNode(mapRef);
 
       let {initialCenter, zoom} = this.props;
-      const {lat, lng} = initialCenter;
       const center = new maps.LatLng(lat, lng);
       const mapConfig = Object.assign({}, {
         center: center,
         zoom: zoom
-      })
+      });
       this.map = new maps.Map(node, mapConfig);
     }
   }
@@ -34,10 +48,10 @@ export class Map extends React.Component {
 }
 
 Map.propTypes = {
-google: React.PropTypes.object,
-zoom: React.PropTypes.number,
-initialCenter: React.PropTypes.object
-}
+  google: React.PropTypes.object,
+  zoom: React.PropTypes.number,
+  initialCenter: React.PropTypes.object
+};
 
 Map.defaultProps = {
   zoom: 13,
@@ -46,4 +60,6 @@ Map.defaultProps = {
     lat: 37.774929,
     lng: -122.419416
   }
-}
+};
+
+export default Map;
