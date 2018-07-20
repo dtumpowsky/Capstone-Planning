4413ac5d7d174a129f398d26176aede8620fcfbe
David Tumpowsky <tumpowskydavid@gmail.com>
2018-07-20 11:04:05 -0700

    google map now renders in app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import Error404 from './Error404';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
 import { v4 } from 'uuid';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
 
 class App extends React.Component {
 
@@ -19,6 +21,12 @@ class App extends React.Component {
           <Route exact path='/' />
           <Route component={Error404} />
         </Switch>
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={'Current location'} />
+          <InfoWindow onClose={this.onInfoWindowClose}>
+
+        </InfoWindow>
+      </Map>
         <div style={{margin: '19px auto 0', width: 142}}>
           <a href="https://medium.com/" target="_blank">
             <div className="box">
@@ -34,4 +42,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyCzmdObLsLWto9B5Uy_eiKtlX95gZopT8w')
+})(App);
