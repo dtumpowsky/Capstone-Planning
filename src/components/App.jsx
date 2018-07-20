c48cf88fb57cc6f38c0913829e9af4314a6dc511
David Tumpowsky <tumpowskydavid@gmail.com>
2018-07-20 12:52:33 -0700

    map styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import Error404 from './Error404';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
 import { v4 } from 'uuid';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import GoogleMap from './GoogleMap';
 
 
@@ -18,23 +17,16 @@ class App extends React.Component {
         <div className='jumbotron'>
           <Header/>
         </div>
-        <Switch>
-          <Route exact path='/' />
-          <Route component={Error404} />
-        </Switch>
         <div>
           <GoogleMap/>
-        </div>
-        <div style={{margin: '19px auto 0', width: 142}}>
-          <a href="https://medium.com/" target="_blank">
-            <div className="box">
-            </div>
-          </a>
         </div>
         <div>
           <Footer/>
         </div>
-
+        <Switch>
+          <Route exact path='/' />
+          <Route component={Error404} />
+        </Switch>
       </div>
     );
   }
--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import PropTypes from 'prop-types';
+
+const style = {
+  textAlign: 'center',
+  maxHeight: '90%',
+  maxWidth: '85%',
+};
 
 class GoogleMap extends React.Component {
 
   render() {
     return (
       <div>
-        <Map google={this.props.google} zoom={14}>
+        <Map google={this.props.google} zoom={14} className='map-styles' style={style}>
           <Marker onClick={this.onMarkerClick} name={'Current location'} />
           <InfoWindow onClose={this.onInfoWindowClose}>
           </InfoWindow>
